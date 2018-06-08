2e785a5b953f61e5930e33caf28a1c505fdb8ef4
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-07 23:11:10 -0500

    add manga factory

---
+++ client/factories/manga.factory.js
@@ -0,0 +1,66 @@
+mangaReader.factory('mangaFactory', function () {
+  return {
+    list: [
+      {
+        name: 'Tactics',
+        cover: 'https://placekitten.com/400/400?image=1',
+        pages: 76,
+      },
+      {
+        name: 'Quality',
+        cover: 'https://placekitten.com/400/400?image=2',
+        pages: 92,
+      },
+      {
+        name: 'Metrics',
+        cover: 'https://placekitten.com/400/400?image=3',
+        pages: 44,
+      },
+      {
+        name: 'Interactions',
+        cover: 'https://placekitten.com/400/400?image=4',
+        pages: 80,
+      },
+      {
+        name: 'Data',
+        cover: 'https://placekitten.com/400/400?image=5',
+        pages: 41,
+      },
+      {
+        name: 'Communications',
+        cover: 'https://placekitten.com/400/400?image=6',
+        pages: 63,
+      },
+      {
+        name: 'Applications',
+        cover: 'https://placekitten.com/400/400?image=7',
+        pages: 97,
+      },
+      {
+        name: 'Communications',
+        cover: 'https://placekitten.com/400/400?image=8',
+        pages: 22,
+      },
+      {
+        name: 'Usability',
+        cover: 'https://placekitten.com/400/400?image=9',
+        pages: 18,
+      },
+      {
+        name: 'Research',
+        cover: 'https://placekitten.com/400/400?image=10',
+        pages: 34,
+      },
+      {
+        name: 'Interactions',
+        cover: 'https://placekitten.com/400/400?image=11',
+        pages: 79,
+      },
+      {
+        name: 'Factors',
+        cover: 'https://placekitten.com/400/400?image=12',
+        pages: 50,
+      },
+    ],
+  };
+})
