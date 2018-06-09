f0077ffab1c2aff3b6b2c81d805e61b5aaf20f96
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 14:07:14 -0500

    add ui router

--- client/app.js
+++ client/app.js
@@ -1,1 +1,26 @@
-const mangaReader = angular.module('manga-reader', [], function() {});
+const mangaReader = angular.module('manga-reader', ['ui.router']);
+
+mangaReader.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+      .state('home', {
+          url: '/home',
+          component: 'library',
+      })
+      .state('library', {
+          url: '/library',
+          component: 'library',
+      })
+      .state('add', {
+          url: '/add',
+          component: 'add',
+      })
+      .state('search', {
+          url: '/search',
+          component: 'search',
+      })
+
+
+});
