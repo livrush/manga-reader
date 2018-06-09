4501e25392f1b7689530636db3d4cb89e1cbd70b
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 14:47:10 -0500

    display directories and append path on click

--- client/components/add/add.component.js
+++ client/components/add/add.component.js
@@ -1,18 +1,24 @@
 mangaReader.component('add', {
   controllerAs: 'add',
-  controller: function() {
+  controller: function($scope) {
     const add = this;
     add.path = os.homedir();
     add.directories = [];
     add.files = [];
     add.searchDirectory = function() {
       add.files = [];
-      fs.readdir(add.directory, function(err, data) {
+      fs.readdir(add.path, function(err, files) {
         if (err) return console.error(err);
-        console.log(data);
-
+        const x = files
+          .filter(file => fs.lstatSync(path.join(add.path, file)).isDirectory())
+          .filter(file => file[0] !== '.')
+          .forEach(file => add.files.push(file));
+        $scope.$apply();
       });
-    }
+    };
+    add.appendDirectory = function(file) {
+      add.path = path.join(add.path, file);
+    };
   },
   templateUrl: './components/add/add.template.html'
 });
