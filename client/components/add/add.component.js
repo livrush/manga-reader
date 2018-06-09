a984359e1161938e2c00efd5e6bee0b40a1bcac4
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 15:30:43 -0500

    update files and directories

--- client/components/add/add.component.js
+++ client/components/add/add.component.js
@@ -5,20 +5,35 @@ mangaReader.component('add', {
     add.path = os.homedir();
     add.directories = [];
     add.files = [];
+
+    add.popDirectory = function() {
+      add.path = path.dirname(add.path);
+      this.searchDirectory();
+    };
+
+    add.appendDirectory = function(file) {
+      add.path = path.join(add.path, file);
+      this.searchDirectory();
+    };
+
     add.searchDirectory = function() {
       add.files = [];
+      add.directories = [];
       fs.readdir(add.path, function(err, files) {
         if (err) return console.error(err);
-        const x = files
+        files
           .filter(file => fs.lstatSync(path.join(add.path, file)).isDirectory())
+          .filter(file => file[0] !== '.')
+          .forEach(file => add.directories.push(file));
+        files
+          .filter(file => !fs.lstatSync(path.join(add.path, file)).isDirectory())
           .filter(file => file[0] !== '.')
           .forEach(file => add.files.push(file));
         $scope.$apply();
       });
     };
-    add.appendDirectory = function(file) {
-      add.path = path.join(add.path, file);
-    };
+
+    add.searchDirectory();
   },
   templateUrl: './components/add/add.template.html'
 });
