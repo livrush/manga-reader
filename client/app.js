b09b798c3e3aab900b00f198f39b4375015a05c9
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 14:47:17 -0500

    import lodash, path

--- client/app.js
+++ client/app.js
@@ -1,28 +1,30 @@
 const fs = require('fs');
 const os = require('os');
+const path = require('path');
+const lodash = require('lodash');
 const mangaReader = angular.module('manga-reader', ['ui.router']);
 
-mangaReader.config(function($stateProvider, $urlRouterProvider) {
+mangaReader.config(function ($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/home');
 
   $stateProvider
-      .state('home', {
-          url: '/home',
-          component: 'library',
-      })
-      .state('library', {
-          url: '/library',
-          component: 'library',
-      })
-      .state('add', {
-          url: '/add',
-          component: 'add',
-      })
-      .state('search', {
-          url: '/search',
-          component: 'search',
-      })
+    .state('home', {
+      url: '/home',
+      component: 'library',
+    })
+    .state('library', {
+      url: '/library',
+      component: 'library',
+    })
+    .state('add', {
+      url: '/add',
+      component: 'add',
+    })
+    .state('search', {
+      url: '/search',
+      component: 'search',
+    })
 
 
 });
