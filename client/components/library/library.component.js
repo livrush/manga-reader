68717735c04aeae876f8d02b5d8c2d40f1ec58da
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 12:58:48 -0500

    add temporary api call to kitsu

--- client/components/library/library.component.js
+++ client/components/library/library.component.js
@@ -1,6 +1,25 @@
+var kitsu = require("kitsu")
+
+const api = new kitsu();
+
+
 mangaReader.component('library', {
   controllerAs: 'library',
   controller: function(mangaFactory) {
+    api.get('anime', {
+      page: { limit: 20 },
+      sort: 'popularityRank',
+    })
+      .then(function({ data }) {
+        library.manga = data.map(anime => {
+          return {
+            cover: anime.posterImage.medium,
+            name: anime.canonicalTitle,
+            pages: anime.episodeCount,
+          };
+        })
+      })
+      .catch(console.log)
     const library = this;
     library.manga = mangaFactory.list;
     library.selected = {};
