9237176de2826cb8d1d317968c837679ca1b142a
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 19:06:56 -0500

    clean

--- client/components/library/library.component.js
+++ client/components/library/library.component.js
@@ -3,16 +3,8 @@ mangaReader.component('library', {
   controller: function(mangaFactory, $scope) {
     const library = this;
 
-    const getIndex = function() {
-      fs.readFile(libraryIndexPath, 'utf8', function(err, indexContents) {
-        const list = JSON.parse(indexContents);
-        library.manga = list.concat(list).concat(list).concat(list);
-        library.selected = list[0];
-        $scope.$apply();
-      });
-    }
-
     library.selected = {};
+
     library.select = (selected) => {
       library.selected = selected;
     }
