856601e47e139de53119efd41f2c98c622aa9c0e
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-10 12:18:22 -0500

    add key events for viewer

--- client/components/viewer/viewer.component.js
+++ client/components/viewer/viewer.component.js
@@ -3,23 +3,51 @@ mangaReader.component('viewer', {
     filePath: '@',
   },
   controllerAs: 'viewer',
-  controller: function($scope, mangaFactory) {
+  controller: function ($scope, mangaFactory) {
     const viewer = this;
 
     viewer.test = (val) => console.log('Working!', val);
 
     viewer.pages = [];
     viewer.index = 0;
-    viewer.back = () => { if (viewer.index > 0) viewer.index--; };
-    viewer.forward = () => { if (viewer.index < viewer.pages.length - 1) viewer.index++; };
+    viewer.back = () => {
+      if (viewer.index > 0) viewer.index--;
+    };
+    viewer.forward = () => {
+      if (viewer.index < viewer.pages.length - 1) viewer.index++;
+    };
 
-    viewer.$onInit = function() {
+    viewer.$onInit = function () {
       const filePath = mangaFactory.getFilePath();
       mangaFactory.getCollection(filePath)
-      .then(function(blobUrls) {
-        viewer.pages = blobUrls;
-        $scope.$apply();
-      });
+        .then(function (blobUrls) {
+          viewer.pages = blobUrls;
+          $scope.$apply();
+        });
+
+      document.onkeydown = checkKey;
+
+      function checkKey(e) {
+
+        e = e || window.event;
+        console.log(e);
+        if (e.keyCode == '38') {
+          // up arrow
+        } else if (e.keyCode == '40') {
+          // down arrow
+        } else if (e.keyCode == '37') {
+          // left arrow
+          document.getElementById('viewer-back').click();
+        } else if (e.keyCode == '39') {
+          // right arrow
+          document.getElementById('viewer-forward').click();
+        }
+
+      }
+    }
+
+    viewer.$onDestroy = function () {
+      document.onkeydown = null;
     }
   },
   templateUrl: './components/viewer/viewer.template.html'
