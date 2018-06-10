d20184f88bcb3fb9af77cfa62c26923d56811282
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-10 01:21:24 -0500

    update click function

--- client/components/info/list/info-list.component.js
+++ client/components/info/list/info-list.component.js
@@ -1,9 +1,15 @@
 mangaReader.component('infoList', {
   bindings: {
+    manga: '<',
     numbers: '<',
     type: '<',
   },
   controllerAs: 'list',
-  controller: function(mangaFactory) {},
+  controller: function(mangaFactory) {
+    const list = this;
+    list.click = function(file) {
+      mangaFactory.setFilePath(path.join(__dirname, './.manga', list.manga, file));
+    }
+  },
   templateUrl: './components/info/list/info-list.template.html'
 });
