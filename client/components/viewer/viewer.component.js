ec4fd500890c7e9930aedde387d7063b465650c7
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 19:36:47 -0500

    display pages with ability to cycle through!!

--- client/components/viewer/viewer.component.js
+++ client/components/viewer/viewer.component.js
@@ -1,7 +1,22 @@
 mangaReader.component('viewer', {
-  controllerAs: 'selected',
-  controller: function() {
+  controllerAs: 'viewer',
+  controller: function($scope, mangaFactory) {
+    const viewer = this;
+    const filePath = '/Users/liv/Itoshi-no-Nekokke-ch30.zip'
 
+    viewer.pages = [];
+    viewer.index = 0;
+    viewer.forward = function() {
+      viewer.index++;
+    };
+
+    viewer.$onInit = function() {
+      mangaFactory.getCollection(filePath)
+      .then(function(blobUrls) {
+        viewer.pages = blobUrls;
+        $scope.$apply();
+      });
+    }
   },
   templateUrl: './components/viewer/viewer.template.html'
 });
