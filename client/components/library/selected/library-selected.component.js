a58ca88b3cec72a48b5cfde48b2dc3283b674efa
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 21:31:54 -0500

    correct displayed title

--- client/components/library/selected/library-selected.component.js
+++ client/components/library/selected/library-selected.component.js
@@ -5,6 +5,9 @@ mangaReader.component('librarySelected', {
   controllerAs: 'selected',
   controller: function(mangaFactory) {
     const selected = this;
+    selected.displayTitle = function(title) {
+      return title.replace(/-/g, ' ');
+    }
   },
   templateUrl: './components/library/selected/library-selected.template.html'
 });
