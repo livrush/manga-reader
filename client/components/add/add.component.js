6ff968037cafda9f4d7ea32a27e4dd230ec4b33f
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 19:36:26 -0500

    adjust for new place of function

--- client/components/add/add.component.js
+++ client/components/add/add.component.js
@@ -1,27 +1,35 @@
 mangaReader.component('add', {
   controllerAs: 'add',
-  controller: function($scope) {
+  controller: function($scope, mangaFactory) {
     const add = this;
     add.path = os.homedir();
     add.selected = {};
     add.folders = [];
     add.files = [];
 
+    // add.selectFile = function(file) {
+    //   const filePath = path.join(add.path, file);
+    //   fs.readFile(filePath, function(err, data) {
+    //     if (err) throw err;
+    //     JSZip.loadAsync(data).then(function ({ files }) {
+    //       const onlyFiles = lodash.filter(files, file => !file.dir);
+    //       const file = onlyFiles[10];
+    //       file.async('blob').then(function(res) {
+    //         var urlCreator = window.URL || window.webkitURL;
+    //         var imageUrl = urlCreator.createObjectURL( res );
+    //         var img = document.querySelector( "#photo" );
+    //         img.src = imageUrl;
+    //       });
+    //     });
+    //   });
+    // };
+
     add.selectFile = function(file) {
       const filePath = path.join(add.path, file);
-      fs.readFile(filePath, function(err, data) {
-        if (err) throw err;
-        JSZip.loadAsync(data).then(function ({ files }) {
-          const onlyFiles = lodash.filter(files, file => !file.dir);
-          const file = onlyFiles[10];
-          file.async('blob').then(function(res) {
-            var urlCreator = window.URL || window.webkitURL;
-            var imageUrl = urlCreator.createObjectURL( res );
-            var img = document.querySelector( "#photo" );
-            img.src = imageUrl;
-          });
-        });
-      });
+      mangaFactory.getCollection(filePath)
+        .then(function(res) {
+          console.log(res);
+        })
     };
 
     add.popFolder = function() {
