29eb0685701952051a5da0e1a92083aaf2354ca7
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-09 19:36:14 -0500

    move get collection function to factory

--- client/factories/manga.factory.js
+++ client/factories/manga.factory.js
@@ -9,7 +9,37 @@ mangaReader.factory('mangaFactory', function () {
     });
   }
 
+  const getCollection = function(filePath) {
+    console.log(filePath)
+    return new Promise(function(res, rej) {
+      fs.readFile(filePath, function(err, data) {
+        if (err) rej(err);
+
+        JSZip.loadAsync(data).then(function ({ files }) {
+          const onlyFiles = lodash
+            .chain(files)
+            .filter(file => !file.dir)
+            .filter(file => path.extname(file.name) === '.png')
+            .filter(file => !console.log(file.name))
+            .filter(file => !file.name.includes('__MACOSX'))
+            .map(file => {
+              return file.async('blob').then(function(blob) {
+                var urlCreator = window.URL || window.webkitURL;
+                return urlCreator.createObjectURL(blob);
+              });
+            })
+            .value();
+          console.log(onlyFiles);
+          const pAll = Promise.all(onlyFiles);
+          res(pAll);
+        });
+
+      });
+    });
+  };
+
   return {
     getIndex,
+    getCollection,
   };
 })
