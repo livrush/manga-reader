9737c982cb5a0a1233bb2fb15a5fd46fc50a92c5
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-13 12:17:11 -0500

    add selected index methods

--- client/factories/index.factory.js
+++ client/factories/index.factory.js
@@ -1,4 +1,7 @@
-mangaReader.factory('mangaFactory', function () {
+mangaReader.factory('indexFactory', function () {
+  const libraryPath = path.join(__dirname, '/.manga');
+  const libraryIndexPath = path.join(libraryPath, 'index.json');
+
   const getIndex = function () {
     return new Promise(function (res, rej) {
       fs.readFile(libraryIndexPath, 'utf8', function (err, indexContents) {
@@ -7,13 +10,46 @@ mangaReader.factory('mangaFactory', function () {
     });
   };
 
+
   const updateIndex = function (mediaName, updatedInfo) {
     getIndex().then(index => {
 
     });
-  }
+  };
+
+  const getSelectedIndex = function(mediaName) {
+    const selectedIndexPath = path.join(libraryPath, mediaName, 'index.json');
+    return new Promise(function (res, rej) {
+      fs.readFile(selectedIndexPath, 'utf8', function (err, indexContents) {
+        if (indexContents) res(JSON.parse(indexContents));
+        else res(indexContents);
+        rej(err);
+      });
+    });
+  };
+
+  const updateSelectedIndex = function(mediaName, updatedInfo) {
+    const selectedIndexPath = path.join(libraryPath, mediaName, 'index.json');
+    return new Promise(function (res, rej) {
+      fs.readFile(selectedIndexPath, 'utf8', function (err, indexContents) {
+        let index;
+        if (updatedInfo) {
+          if (indexContents && indexContents !== 'undefined') index = lodash.extend(JSON.parse(indexContents), updatedInfo);
+          else index = updatedInfo;
+        } else {
+          index = updatedInfo;
+        }
+        console.log(index);
+        fs.writeFile(selectedIndexPath, JSON.stringify(index), function(err) { if (err) throw err });
+      });
+    });
+  };
+
 
   return {
     getIndex,
+    updateIndex,
+    getSelectedIndex,
+    updateSelectedIndex,
   };
 })
