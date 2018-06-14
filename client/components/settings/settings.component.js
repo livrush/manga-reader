0e206f94499e67c7cc2c52b0f01c44962645ecc5
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-14 11:57:35 -0500

    add settings component

---
+++ client/components/settings/settings.component.js
@@ -0,0 +1,8 @@
+mangaReader.component('settings', {
+  controllerAs: 'settings',
+  controller: function($scope, mangaFactory) {
+    const settings = this;
+
+  },
+  templateUrl: './components/settings/settings.template.html'
+});
