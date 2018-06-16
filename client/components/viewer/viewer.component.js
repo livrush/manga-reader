597f4db90007c1b69414d4eb0d9e56a5a7c697ff
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-16 13:04:37 -0500

    hide img/canvas tag depending on file type

--- client/components/viewer/viewer.component.js
+++ client/components/viewer/viewer.component.js
@@ -7,7 +7,8 @@ mangaReader.component('viewer', {
 
     viewer.test = (val) => console.log('Working!', val);
 
-    viewer.pages = [];
+    viewer.pages = [ './assets/placeholder.png' ];
+    viewer.pdfPages = null;
     viewer.index = 0;
     viewer.back = () => {
       if (viewer.index > 0) viewer.index--;
@@ -17,6 +18,7 @@ mangaReader.component('viewer', {
     viewer.forward = () => {
       if (viewer.index < viewer.pages.length - 1) viewer.index++;
       if (viewer.index === viewer.pages.length - 1) { console.log('end'); }
+      displayPdfPage(viewer.pdfPages, viewer.index);
       viewer.updateIndex();
     };
 
@@ -34,15 +36,16 @@ mangaReader.component('viewer', {
 
       viewer.index = selectedFile.currentPage;
       viewer.filePath = selectedFile.currentFile;
+
+      viewer.fileType = path.extname(viewer.filePath);
+
       mangaFactory.getCollection(selectedFile.currentFile)
         .then(function (collection) {
           const collectionItemType = typeof collection[0];
           switch(collectionItemType) {
             case 'object':
-              collection[0].render({
-                canvasContext: document.getElementById('pdf-container').getContext('2d'),
-                viewport: collection[0].getViewport(1.5),
-              });
+              viewer.pages = collection;
+              displayPdfPage(collection, 0);
               break;
             case 'string':
               viewer.pages = collection;
@@ -54,24 +57,45 @@ mangaReader.component('viewer', {
         });
 
       document.onkeydown = checkKey;
-
-      function checkKey(e) {
-        e = e || window.event;
-        switch (e.keyCode) {
-          case  37:
-          case '37':
-            document.getElementById('viewer-back').click();
-            break;
-          case  39:
-          case '39':
-            document.getElementById('viewer-forward').click();
-            break;
-        };
-      }
     }
 
     viewer.$onDestroy = function () {
       document.onkeydown = null;
+    }
+
+    function checkKey(e) {
+      e = e || window.event;
+      switch (e.keyCode) {
+        case  37:
+        case '37':
+          document.getElementById('viewer-back').click();
+          break;
+        case  39:
+        case '39':
+          document.getElementById('viewer-forward').click();
+          break;
+      };
+    }
+
+    function displayPdfPage(pages, index) {
+      const page = pages[index];
+      console.log(pages.length, index, page);
+      var scale = 1.5;
+      var viewport = page.getViewport(scale);
+
+      // Prepare canvas using PDF page dimensions.
+      var canvas = document.getElementById('pdf-container');
+      var context = canvas.getContext('2d');
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+
+      // Render PDF page into canvas context.
+      var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+      };
+
+      page.render(renderContext);
     }
   },
   templateUrl: './components/viewer/viewer.template.html'
