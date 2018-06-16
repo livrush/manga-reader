58e91158a5dc5b9fc708b26adb619145d287a0ad
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-16 11:24:10 -0500

    begin separating file handlers

--- client/factories/manga.factory.js
+++ client/factories/manga.factory.js
@@ -18,6 +18,19 @@ mangaReader.factory('mangaFactory', function () {
   }
 
   const getCollection = function (filePath) {
+    const fileType = path.extname(filePath);
+    console.warn('FILE TYPE IS ', fileType);
+    switch(fileType) {
+      case '.zip':
+      case '.cbz':
+        return handleZipFile(filePath);
+        break;
+      case '.pdf':
+        return handlePdfFile(filePath);
+    }
+  };
+
+  const handleZipFile = function (filePath) {
     return new Promise(function (res, rej) {
       fs.readFile(filePath, function (err, data) {
         if (err) rej(err);
@@ -41,7 +54,6 @@ mangaReader.factory('mangaFactory', function () {
             .value();
             // .sortBy(file => file.name)
             // .filter(file => !console.log(file.name));
-
           onlyFiles = onlyFiles
             .sort(cmpStringsWithNumbers)
             // .filter((file) => !console.log(file.name))
@@ -55,10 +67,63 @@ mangaReader.factory('mangaFactory', function () {
           const pAll = Promise.all(onlyFiles);
           res(pAll);
         });
-
       });
     });
-  };
+  }
+
+  const handlePdfFile = function (filePath) {
+    return new Promise(function (res, rej) {
+      pdfjsLib.getDocument(filePath).then(function (doc) {
+        var numPages = doc.numPages;
+        console.log('# Document Loaded');
+        console.log('Number of Pages: ' + numPages);
+        console.log();
+
+        // var lastPromise; // will be used to chain promises
+        // lastPromise = doc.getMetadata().then(function (data) {
+        //   console.log('# Metadata Is Loaded');
+        //   console.log('## Info');
+        //   console.log(JSON.stringify(data.info, null, 2));
+        //   console.log();
+        //   if (data.metadata) {
+        //     console.log('## Metadata');
+        //     console.log(JSON.stringify(data.metadata.getAll(), null, 2));
+        //     console.log();
+        //   }
+        // });
+
+        // var loadPage = function (pageNum) {
+        //   return doc.getPage(pageNum).then(function (page) {
+        //     console.log('# Page ' + pageNum);
+        //     var viewport = page.getViewport(1.0 /* scale */);
+        //     console.log('Size: ' + viewport.width + 'x' + viewport.height);
+        //     console.log();
+        //     return page.getTextContent().then(function (content) {
+        //       // Content contains lots of information about the text layout and
+        //       // styles, but we need only strings at the moment
+        //       var strings = content.items.map(function (item) {
+        //         return item.str;
+        //       });
+        //       console.log('## Text Content');
+        //       console.log(strings.join(' '));
+        //     }).then(function () {
+        //       console.log();
+        //     });
+        //   })
+        // };
+        // // Loading of the first page will wait on metadata and subsequent loadings
+        // // will wait on the previous pages.
+        // for (var i = 1; i <= numPages; i++) {
+        //   lastPromise = lastPromise.then(loadPage.bind(null, i));
+        // }
+        // return lastPromise;
+      }).then(function () {
+        console.log('# End of Document');
+      }, function (err) {
+        console.error('Error: ' + err);
+      });
+    });
+  }
 
   const getSomeFromPath = function (filePath, isDirectory) {
     return new Promise(function (res, rej) {
