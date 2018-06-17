b4ddf93220e335d04efe18275aff912b7f72ef8a
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-17 18:23:20 -0500

    update sanitization process

--- client/factories/library.factory.js
+++ client/factories/library.factory.js
@@ -5,7 +5,7 @@ mangaReader.factory('libraryFactory', function () {
   function add(selectedInformation) {
     console.log(selectedInformation);
 
-    sanitizeDestinationPath(selectedInformation)
+    return sanitizeDestinationPath(selectedInformation)
       .then(checkDirectory)
       .catch(makeDirectory)
       .then(copyFile)
@@ -26,14 +26,16 @@ mangaReader.factory('libraryFactory', function () {
   }) {
     return new Promise(function(res, rej) {
       const sanitizedNumber = number.toString().padStart(3, '0');
+      const sanitizedSeriesName = changeCase.paramCase(series)
       const extension = path.extname(fileName);
       const newFileName = `${category}-${sanitizedNumber}${extension}`
-      const destinationPath = path.join(libraryPath, series.toLowerCase(), newFileName);
+      const destinationPath = path.join(libraryPath, sanitizedSeriesName, newFileName);
       res([ filePath, destinationPath ]);
     });
   }
 
   function checkDirectory([ filePath, destinationPath ]) {
+    console.warn('checkDirectory');
     return new Promise(function(res, rej) {
       fs.exists(path.dirname(destinationPath), (exists) => {
         exists ? res([ filePath, destinationPath ]) : rej([ filePath, destinationPath ]);
@@ -42,14 +44,16 @@ mangaReader.factory('libraryFactory', function () {
   }
 
   function makeDirectory([ filePath, destinationPath ]) {
+    console.warn('makeDirectory');
     return new Promise(function(res, rej) {
       fs.mkdir(path.dirname(destinationPath), (err, success) => {
-        err ? rej(err) : rej([ filePath, destinationPath ]);
+        err ? rej(err) : res([ filePath, destinationPath ]);
       });
     });
   }
 
   function copyFile([ filePath, destinationPath ]) {
+    console.warn('copyFile');
     return new Promise(function(res, rej) {
       fs.copyFile(filePath, destinationPath, function(error, response) {
         if (error) rej(error);
