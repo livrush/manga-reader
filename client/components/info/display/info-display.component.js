2ee3e47663aed048ddc47db0da20dd3b92481591
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-06-17 21:47:27 -0500

    able to edit and update index

--- client/components/info/display/info-display.component.js
+++ client/components/info/display/info-display.component.js
@@ -1,11 +1,31 @@
 mangaReader.component('infoDisplay', {
   controllerAs: 'display',
-  controller: function(mediaFactory) {
+  controller: function($scope, mediaFactory, indexFactory) {
     const display = this;
+    display.data = {};
 
     display.$onInit = function() {
       display.imageUrl = mediaFactory.getThumbnail(display.manga);
-    }
+      indexFactory.getIndexByTitle(display.manga)
+        .then(function(data) {
+          display.data = data;
+          $scope.$apply();
+        });
+      console.log(display);
+    };
+
+    display.editing = false;
+
+    display.editingStart = function() {
+      display.editing = true;
+      console.log('start', display.data);
+    };
+
+    display.editingStop = function() {
+      display.editing = false;
+      console.log('stop', display.data);
+      indexFactory.updateIndexByTitle(display.manga, display.data);
+    };
   },
   bindings: {
     manga: '<',
